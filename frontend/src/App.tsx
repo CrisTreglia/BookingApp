43df777364bf1c60366ef1a8cf890db605778dc6
Cristofer Treglia <cristofertreglia@outlook.com>
2024-08-03 21:10:07 -0600

    Creating register page and some validations

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import {
   Routes 
 } from 'react-router-dom';
 import Layout from './layouts/Layout'
+import Register from './pages/Register';
 
 function App() {
   return (
@@ -21,6 +22,10 @@ function App() {
             <p>Search Page</p>
           </Layout>
         } />
+        <Route path="/register" element={
+          <Layout><Register /></Layout>
+        }>
+        </Route>
         <Route path="*" element={<Navigate to="/" />}/>
       </Routes>
     </Router>
